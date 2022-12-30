eb75029b2452959527387b81c2c4abc4fae1da40
Fernando <fh1273509@gmail.com>
2022-12-29 21:19:04 -0300

    rutas

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
+const { request, response } = require('express');
 const express = require('express'); //instanciamos express luego de instalarlo
 const app = express();
 
+//rutas:
 app.get('/', (requeste, response) =>{
 
     response.end('Hola Fer'); 
+
+})
+
+//vamos a agregar la ruta para el login:
+
+app.get('/login', (request, response) => {
+
+    response.end('aqui va el login');
+
+});
+
+app.get('*', (request, response)=>{
+
+    response.end('archivo no encontrado');
 
 })
 
